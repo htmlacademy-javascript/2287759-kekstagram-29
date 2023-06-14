e130a17cab1814a78646ac6d30a6fa1a79695b50
Марина <marinashakirovakim@gmail.com>
2023-06-14 03:09:01 +0300

    functions

--- js/functions.js
+++ js/functions.js
@@ -1,73 +1,15 @@
-let string = prompt("Введите строку");
-let maxlength = Number(prompt("Введите максимальную длинну"));
-function comparison(){
-  if(string.length <= maxlength){
-    alert("Длина строки меньше максимальной");
-  }
-  else {alert("Длина строки больше максимальной");}
-}
-comparison(string.length, maxlength);
 
-let stringCompressed = string.replaceAll(" ", "");
-let stringUp = stringCompressed.toUpperCase();
-function isItPalindrome(){
-let newString = "";
-for(let i=(stringUp.length-1); i>=0; i--){
-newString = newString + stringUp[i];
-}
-if(newString === stringUp)
-{
-  alert("Это палиндром");
-}
-else{
-    alert("Это НЕ палиндром");
-  }
-}
-isItPalindrome(stringUp);
+const comparison = (string, maxlength) =>string.length <= maxlength;
+console.log(comparison('just like that', 57));
 
-function numberInString(){
-  let number="";
-  for(let i=0; i<=(stringUp.length-1); i++){
-    switch(stringUp[i]){
-      case "0":
-        if(number!=""){
-      number=number + stringUp[i];}
-      break;
-      case "1":
-      number=number + stringUp[i];
-      break;
-      case "2":
-      number=number + stringUp[i];
-      break;
-      case "3":
-      number=number + stringUp[i];
-      break;
-      case "4":
-      number=number + stringUp[i];
-      break;
-      case "5":
-      number=number + stringUp[i];
-      break;
-      case "6":
-      number=number + stringUp[i];
-      break;
-      case "7":
-      number=number + stringUp[i];
-      break;
-      case "8":
-      number=number + stringUp[i];
-      break;
-      case "9":
-      number=number + stringUp[i];
-      break;
-      default: break;
-  }
-  }
-  if(number==""){
-    alert("В строке нет чисел");
-  }
-  else{
-    alert(number);
-  }
+const isItPalindrome = (string)=>{
+  let stringCompressed=string.replaceAll(" ", "");
+  let stringUp=stringCompressed.toUpperCase();
+  let newString = "";
+  for(let i=(stringUp.length-1); i>=0; i--){
+    newString = newString + stringUp[i];
 }
-numberInString(stringUp);
+return stringUp === newString;
+}
+console.log(isItPalindrome("asdffdjsa"));
+
