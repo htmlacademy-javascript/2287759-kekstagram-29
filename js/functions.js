28db8d2bb0d77eacd87b0216b7b7f8cd6ecdc3da
Марина <marinashakirovakim@gmail.com>
2023-06-16 09:06:19 +0300

    правки

--- js/functions.js
+++ js/functions.js
@@ -3,13 +3,13 @@ const comparison = (string, maxlength) =>string.length <= maxlength;
 console.log(comparison('just like that', 57));
 
 const isItPalindrome = (string)=>{
-  let stringCompressed=string.replaceAll(" ", "");
-  let stringUp=stringCompressed.toUpperCase();
-  let newString = "";
-  for(let i=(stringUp.length-1); i>=0; i--){
+  const stringCompressed = string.replaceAll(' ', '');
+  const stringUp = stringCompressed.toUpperCase();
+  let newString = '';
+  for(let i = (stringUp.length - 1); i >= 0; i--){
     newString = newString + stringUp[i];
-}
-return stringUp === newString;
-}
-console.log(isItPalindrome("asdffdjsa"));
+  }
+  return stringUp === newString;
+};
+console.log(isItPalindrome('asdffdjsa'));
 
